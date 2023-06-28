d9c6f5b99deda32c0fa82e1332a89fadd9e9c800
JPereyraScoutBird <jpereyra@scoutbird.com>
2023-06-27 20:54:01 -0400

    add pages, and sections/

---
+++ src/pages/Home/index.js
@@ -0,0 +1,34 @@
+import Menu from "../../container/Menu";
+
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <Menu />
+      </div>
+      <div style={{ backgroundColor: "#fff", marginTop: "2rem" }}>
+        <section >
+            {/* What is  */}
+        </section>
+        <section >
+            {/* Why us */}
+        </section>
+        <section>
+            {/* Business */}
+        </section>
+        <section>
+            {/* Market/Sports */}
+        </section>
+        <section>
+            {/* {plans} */}
+        </section>
+        <section>
+            {/* {contant us} */}
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import PATH_LIST from './contant';
+import Pages from '../pages';
+
+const route_list = [
+    {
+      path: PATH_LIST.HOME,
+      element: <Pages.Home />,
+    },
+  ]
+
+export default route_list
