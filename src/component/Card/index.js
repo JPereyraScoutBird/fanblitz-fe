d0b187c755b493088a5a3711b4f0ab4446ff3f92
JPereyraScoutBird <jpereyra@scoutbird.com>
2023-08-10 23:07:23 -0700

    test picture

--- src/component/Card/index.js
+++ src/component/Card/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardBody, CardFooter, CardImg, CardTitle } from "reactstrap";
 import './style.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,8 @@ import { faCalendar, faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
 
 function CardComponent (props) {
     const [error, setError] = useState(false)
+
+    const [finalImageURL, setFinalImageURL] = useState(null);
 
     const {
         imageSrc,
@@ -18,11 +20,31 @@ function CardComponent (props) {
         classNameImg
     } = props
 
+
+    useEffect(() => {
+        // const img = new Image();
+        
+        // img.onload = () => {
+        //     const finalURL = img.src;
+        //     setFinalImageURL(finalURL);
+        // };
+        
+        // img.src = imageSrc;
+        fetch(imageSrc, { method: 'HEAD' })
+        .then(response => {
+            const finalURL = response.url;
+            setFinalImageURL(finalURL);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }, [imageSrc]);
+
     if (error != true && imageSrc) {
         return (
             <div className={className}>
                 <Card className={`card_component ${style}`}>
-                    <CardImg className={error ? "error" : classNameImg} onError={() => setError(true)} src={imageSrc || false}/>
+                    <CardImg className={error ? "error" : classNameImg} onError={() => setError(true)} src={finalImageURL || false}/>
                     <CardTitle className="mt-3">
                         <a href={linkTitle}>{title}</a>
                     </CardTitle>
--- src/component/CardProfile/index.js
+++ src/component/CardProfile/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardBody, CardFooter, CardImg, CardTitle } from "reactstrap";
 import './style.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,6 +8,7 @@ import Image from "../../img";
 
 function CardProfileComponent (props) {
     const [error, setError] = useState(false)
+    const [finalImageURL, setFinalImageURL] = useState(null);
 
     const {
         imageSrc,
@@ -20,12 +21,40 @@ function CardProfileComponent (props) {
         classNameImg
     } = props
 
+    // useEffect(() => {
+    //     // const img = new Image();
+        
+    //     // img.onload = () => {
+    //     //     const finalURL = img.src;
+    //     //     setFinalImageURL(finalURL);
+    //     // };
+        
+    //     // img.src = imageSrc;
+    //     fetch(imageSrc, { method: 'GET' })
+    //     .then(response => {
+    //         console.log("img", response)
+    //         const finalURL = response.url;
+            
+    //         setFinalImageURL(finalURL)
+    //         // fetch(finalURL, { method: 'GET' })
+    //         // .then(response2 => {
+    //         //     console.log("response2:", response2)
+    //         //     setFinalImageURL(response2);
+    //         // }).catch(error => {
+    //         //     console.error('Error:', error);
+    //         // });
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+    // }, [imageSrc]);
+
     // if (error != true) {
         return (
             <Link to={link} className={`${className} card-link shadow mb-5 p-2 bg-white rounded`}>
                 <Card className={`card_profile_component ${style}`}>
                     <div>
-                        <CardImg className={classNameImg} onError={() => setError(true)} src={error ? Image.PROFILE : imageSrc}/>
+                        <CardImg className={classNameImg} onError={() => setError(true)} src="https://img.mlbstatic.com/mlb-photos/image/upload/w_213,d_people:generic:headshot:silo:current.png,q_auto:best,f_auto/v1/people/665489/headshot/67/current"/>
                     </div>
                     <div className="w-100">
                         <CardTitle>
