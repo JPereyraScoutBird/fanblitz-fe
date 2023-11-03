ce38945d8a4b18622e41a0cb50a5bf75cbe4a966
JuanGraciano <juanpgracianot@gmail.com>
2023-11-02 22:39:55 -0400

    update nov 6

--- src/pages/cbb/Forecasts/index.js
+++ src/pages/cbb/Forecasts/index.js
@@ -16,7 +16,7 @@ import moment from 'moment'
 function Forecasts() {
 
   const [forecastData, setForecastData] = useState([])
-  const [date, setDate] = useState(moment(new Date('2022-11-08').toLocaleString('en-US', {timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone})))
+  const [date, setDate] = useState(moment(new Date('2023-11-07').toLocaleString('en-US', {timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone})))
 
   // Fetch Data from NewsApi (ComponentDidMount)
   useEffect(() => {
