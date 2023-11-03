852ea75d6f0dd849ea745dc0a54408d29b0ff44d
JuanGraciano <juanpgracianot@gmail.com>
2023-11-03 12:13:48 -0400

    add new filter for player

--- src/pages/cbb/Players/index.js
+++ src/pages/cbb/Players/index.js
@@ -4,12 +4,18 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import PATH_LIST from "../../../routes/constant";
 import SearchTable from "../../../component/SearchTable";
+import Select from 'react-select';
+import './style.css';
 
 function Players() {
     const [teamData, setTeamData] = useState([]);
     const [pitcherData, setPitcherData] = useState([]);
     const [batterData, setBatterData] = useState([]);
     const [showTable, setShowTable] = useState("hitters");
+    const [seasonValue, setSeasonValue] = useState({value: "2023-2024-regular", label: "2023-2024-regular"});
+    const [selectSeason, setSelectSeason] = useState([{value: "", label: ""}]);
+    const [rankingSeason, setRankingSeason] = useState([{value: "2023-2024-regular", label: "2023-2024-regular"}, {value: "2022-2023-regular", label: "2022-2023-regular"}]);
+    const [currentSeason, setCurrentSeason] = useState({value: "2023-2024-regular", label: "2023-2024-regular"}); 
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -18,10 +24,10 @@ function Players() {
                 const response = await axios.get('https://crfh3pd7oi.execute-api.us-east-1.amazonaws.com/devncaa/cbb/stats/players');
                 const jsonObject = JSON.parse(response.data.body)
                 
-                setPitcherData(jsonObject.filter(x => x['position'] == 'P'))
-                setBatterData(jsonObject.filter(x => x['position'] != 'P'))
+                // setPitcherData(jsonObject.filter(x => x['position'] == 'P'))
+                // setBatterData(jsonObject.filter(x => x['position'] != 'P'))
                 setTeamData(jsonObject);
-                console.log("jsonObject", jsonObject.length)
+                console.log("player stats data", jsonObject)
             } catch (error) {
                 console.error('Error getting data:', error);
             }
@@ -35,11 +41,44 @@ function Players() {
     }
 
     const renderTable = () => {
-        return <CustomTable search={true} search_placeholder="Search player name or team on table" search_keys={['full_name']} loading={teamData.length == 0} pagination={true} range={15} header={header} data={teamData} onClick={(user) => onClick(user)}/>;
+        let season = seasonValue == null? "2023-2024-regular": seasonValue.value
+        let data_filter = teamData.filter(x => x['season']==season)
+        console.log("current season", season)
+        console.log("filtrado", data_filter)
+        return <CustomTable search={true} search_placeholder="Search player name or team on table" search_keys={['full_name']} loading={teamData.length == 0} pagination={true} range={15} header={header} data={data_filter} onClick={(user) => onClick(user)}/>;
     };
 
+    const handleFilter = () => {
+        setCurrentSeason(seasonValue)
+    };
+
+    const handleClear = () => {
+        setSeasonValue(null)
+    };
+
+    const renderOptions = () => {
+        return (
+          <div>
+              <div className="dropdown-container">
+              <Select
+                className="dropdown"
+                options={rankingSeason}
+                placeholder="Season"
+                value={seasonValue}
+                onChange={setSeasonValue}
+              />
+            </div>
+            {/* <div className="button-container">
+              <button onClick={handleFilter} className="button">Apply</button>
+              <button onClick={handleClear} className="button">Clear</button>
+            </div> */}
+          </div>
+        );
+        
+      };
+
     const header = {
-        "position": "Position",
+        "position": "Pos.",
         "full_name": "Name",
         "mysportfeeds_abbreviation": "Team",
         "pts_per_game": "PTS",
@@ -55,15 +94,18 @@ function Players() {
         "off_reb": "OFF REB",
         "def_reb": "DEF REB",
         "plus_minus_per_game": "+/-",
-        "games_played": "GAMES"
+        "games_played": "G.",
+        // "season": "Season"
     }
     
     return (
         <div>
         <div style={{ backgroundColor: "#fff", marginTop: "2rem" }}>
             <div style={{ display: "flex", justifyContent: "left", marginBottom: "1rem" }}>
-            <button style={{ border: "none", background: "transparent", padding: "0", marginRight: "1rem", color: "#000" }} >Players</button>
+            <button style={{ border: "none", background: "transparent", padding: "0", marginRight: "1rem", color: "#000", fontWeight: "bold" }} >Players</button>
             </div>
+            {renderOptions()}
+            <br></br>
             {renderTable()}
         </div>
         </div>    
